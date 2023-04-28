84c76e594693987d5baff05acb337cb8368a4e1c
xhifi <shifa.newversion@gmail.com>
2023-04-29 03:38:36 +0500

    Get All Users controller updated with correct middlewares and role checking in routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,20 @@
+const { StatusCodes } = require("http-status-codes");
 const db = require("../utils/db");
+const NotFoundError = require("../errors/NotFoundError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
 
-const getAll = (req, res) => {};
+const getAll = async (req, res) => {
+  if (req.user.role === "client") {
+    throw new UnauthorizedError("Request Elevation from administrator");
+  }
+  const allUsers = await db.query(
+    `SELECT name, email, id, address, created_at, updated_at, role, verification_status FROM users`
+  );
+  if (allUsers.rowCount === 0) {
+    throw new NotFoundError("No User Found");
+  }
+  return res.status(StatusCodes.OK).json(allUsers.rows);
+};
 const deleteAll = (req, res) => {};
 const updateAll = (req, res) => {};
 const getById = (req, res) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const verifyJWT = require("../middlewares/auth/verifyJWT");
+const verifyRoles = require("../middlewares/auth/verifyRoles");
+
 const {
   deleteAll,
   getAll,
@@ -8,7 +11,11 @@ const {
   updateById,
 } = require("../controllers/userController");
 
-router.route("/").get(getAll).put(updateAll).delete(deleteAll);
+router
+  .route("/")
+  .get(verifyJWT, verifyRoles, getAll)
+  .put(updateAll)
+  .delete(deleteAll);
 router.route("/:id").get(getById).put(updateById).delete(deleteById);
 
 module.exports = router;
