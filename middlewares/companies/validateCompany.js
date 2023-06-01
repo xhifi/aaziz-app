90b5157a3c7c65c61344ab587e10effe6d47523c
xhifi <shifa.newversion@gmail.com>
2023-06-01 19:52:05 +0500

    Added middleware to identify and validate a company over Companies House through number from request's body

--- middlewares/companies/validateCompany.js
+++ middlewares/companies/validateCompany.js
@@ -1,14 +1,17 @@
 const fetch = require("node-fetch");
 const BadRequestError = require("../../errors/BadRequestError");
+const db = require("../../utils/db");
+
 const baseUrl = process.env.COMPANIES_HOUSE_BASE_URL;
-const apiKey = process.env.COMPANIES_HOUSE_API_KEY;
-const apiKeyBase64 = Buffer.from(apiKey).toString("base64");
+const apiKey = Buffer.from(process.env.COMPANIES_HOUSE_API_KEY).toString(
+  "base64"
+);
 
 const getCompanyData = async (id) => {
   const res = await fetch(`${baseUrl}/company/${id}`, {
     method: "GET",
     headers: {
-      Authorization: `Basic ${apiKeyBase64}`,
+      Authorization: `Basic ${apiKey}`,
       Accept: "application/vnd.hmrc.1.0+json",
     },
   });
@@ -18,34 +21,26 @@ const getCompanyData = async (id) => {
 };
 
 const validateCompany = async (req, res, next) => {
-  const {
-    name,
-    email,
-    phone_number,
-    website,
-    registration_number,
-    company_number,
-    industry,
-    country,
-    state,
-    city,
-    zip_code,
-    address,
-  } = req.body;
-  if (!company_number) throw new BadRequestError("Provide company number");
+  const { company_number, name, email, phone_number } = req.body;
+
+  if (!name || !email || !phone_number || !company_number) {
+    throw new BadRequestError(
+      "Provide company name and email, phone number and Company number"
+    );
+  }
 
   const companyExists = await db.query(
     `SELECT company_number, name FROM companies WHERE name = $1 OR company_number = $2`,
     [name, company_number]
   );
   if (companyExists.rowCount > 0) {
-    throw new BadRequestError("Company already exists");
+    throw new BadRequestError(
+      `Company with company# ${companyExists.rows[0].company_number} already exists in the system`
+    );
   }
 
-  const companyData = await getCompanyData(company_number);
-  if (companyData.errors && companyData.errors.length > 0)
-    throw new BadRequestError("No company record found in Companies House");
-  req.companyData = companyData;
+  const companiesHouseData = await getCompanyData(company_number);
+  req.companiesHouseValidation = companiesHouseData;
 
   next();
 };
