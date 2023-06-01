6e0fca30893d880b0da03a670910fe4f1cd5b567
xhifi <shifa.newversion@gmail.com>
2023-06-01 19:49:03 +0500

    Created single export file for all errors

---
+++ errors/index.js
@@ -0,0 +1,13 @@
+const BadRequestError = require("./BadRequestError");
+const UnauthorizedError = require("./UnauthorizedError");
+const ExtendedError = require("./ExtendedError");
+const NotFoundError = require("./NotFoundError");
+const ForbiddenRequestError = require("./ForbiddenRequestError");
+
+module.exports = {
+  BadRequestError,
+  UnauthorizedError,
+  ExtendedError,
+  NotFoundError,
+  ForbiddenRequestError,
+};
