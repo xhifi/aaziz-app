e093760f6d12660a2d71e095eaec787a34fb076e
xhifi <shifa.newversion@gmail.com>
2023-06-01 19:53:02 +0500

    routes added for companies controllers along with middlewares

--- routes/companiesRouter.js
+++ routes/companiesRouter.js
@@ -5,6 +5,7 @@ const verifyRoles = require("../middlewares/auth/verifyRoles");
 const {
   getAllCompanies,
   addLocalCompany,
+  deleteCompany,
 } = require("../controllers/companiesController");
 
 const validateCompany = require("../middlewares/companies/validateCompany");
@@ -13,5 +14,7 @@ router
   .route("/")
   .get(verifyJWT, verifyRoles, getAllCompanies)
   .post(verifyJWT, verifyRoles, validateCompany, addLocalCompany);
+
+router.route("/:company_number").delete(verifyJWT, verifyRoles, deleteCompany);
 
 module.exports = router;
